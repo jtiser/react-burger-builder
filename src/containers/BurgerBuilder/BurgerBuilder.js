24f0f1bd2caf338dc41b7d7d43223980f28f372c
Jtiser <julien.tiser@outlook.com>
2017-12-27 12:37:36 +0100

    Displaying and Updating the Burger Price

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -65,7 +65,8 @@ class BurgerBuilder extends Component {
                 <BuildControls
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
-                    disabled={disabledInfo} />
+                    disabled={disabledInfo}
+                    price={this.state.totalPrice} />
             </Aux>
         );
     }
