821d2dce4d9c820d2f06ea6a8c233e8d9cf8eb15
Jtiser <julien.tiser@outlook.com>
2018-01-04 23:40:20 +0100

    Creating a Custom Dynamic Input Component

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,6 +4,7 @@ import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.css';
 import axios from '../../../axios-orders';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state = {
@@ -47,10 +48,10 @@ class ContactData extends Component {
     render() {
         let form = (
             <form>
-                <input className={classes.Input} type='text' name='name' placeholder='Your name' />
-                <input className={classes.Input} type='email' name='email' placeholder='Your email' />
-                <input className={classes.Input} type='text' name='street' placeholder='Street' />
-                <input className={classes.Input} type='text' name='postal' placeholder='Postal Code' />
+                <Input inputtype='input' type='text' name='name' placeholder='Your name' />
+                <Input inputtype='input' type='email' name='email' placeholder='Your email' />
+                <Input inputtype='input' type='text' name='street' placeholder='Street' />
+                <Input inputtype='input' type='text' name='postal' placeholder='Postal Code' />
                 <Button
                     btnType='Success'
                     clicked={this.orderHandler}>ORDER</Button>
