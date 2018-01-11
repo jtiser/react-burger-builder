5a2b1925a412bfc626eab9b55c06ecef2f90466b
Jtiser <julien.tiser@outlook.com>
2018-01-11 19:17:01 +0100

    Adding Order Actions

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -105,15 +105,7 @@ class ContactData extends Component {
             ingredients: this.props.ings,
             price: this.props.price,
             orderData: formData
-        }
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({ loading: false });
-                this.props.history.push('/');
-            })
-            .catch(error => {
-                this.setState({ loading: false });
-            });
+        }        
     }
 
     checkValidity(value, rules) {
