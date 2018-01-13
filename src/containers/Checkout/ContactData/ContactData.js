ddcf9bc8f285f3e2d597145f291741788c42bf5a
Jtiser <julien.tiser@outlook.com>
2018-01-13 18:54:50 +0100

    Sharing the Validation Method

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,7 +8,7 @@ import axios from '../../../axios-orders';
 import Input from '../../../components/UI/Input/Input';
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../../store/actions/index';
-import { updateObject } from '../../../shared/utility';
+import { updateObject, checkValidity } from '../../../shared/utility';
 
 class ContactData extends Component {
     state = {
@@ -113,35 +113,13 @@ class ContactData extends Component {
         this.props.onOrderBurger(order, this.props.token);
     }
 
-    checkValidity(value, rules) {
-        let isValid = true;
-
-        if (!rules) {
-            return true;
-        }
-
-        if (rules.required) {
-            isValid = value.trim() !== '' && isValid;
-        }
-
-        if (rules.minLength) {
-            isValid = value.length >= rules.minLength && isValid;
-        }
-
-        if (rules.maxLength) {
-            isValid = value.length <= rules.maxLength && isValid;
-        }
-
-        return isValid;
-    }
-
     inputChangedHandler = (event, inputIdentifier) => {
         const updatedFormElement = updateObject(this.state.orderForm[inputIdentifier], {
             value: event.target.value,
-            valid: this.checkValidity(event.target.value, this.state.orderForm[inputIdentifier].validation),
+            valid: checkValidity(event.target.value, this.state.orderForm[inputIdentifier].validation),
             touched: true
         });
-        
+
         const updatedOrderForm = updateObject(this.state.orderForm, {
             [inputIdentifier]:updatedFormElement
         });
--- src/shared/utility.js
+++ src/shared/utility.js
@@ -4,3 +4,35 @@ export const updateObject = (oldObject, updateProperties) => {
         ...updateProperties
     };
 };
+
+export const checkValidity = (value, rules) => {
+    let isValid = true;
+
+    if (!rules) {
+        return true;
+    }
+
+    if (rules.required) {
+        isValid = value.trim() !== '' && isValid;
+    }
+
+    if (rules.minLength) {
+        isValid = value.length >= rules.minLength && isValid;
+    }
+
+    if (rules.maxLength) {
+        isValid = value.length <= rules.maxLength && isValid;
+    }
+
+    if (rules.isEmail) {
+        //TODO JTI code this rule
+        isValid = true && isValid;
+    }
+    
+    if (rules.isNumeric) {
+        //TODO JTI code this rule
+        isValid = true && isValid;
+    }
+
+    return isValid;
+}
