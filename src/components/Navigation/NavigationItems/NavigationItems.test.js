0e9720576cbf0560e9ba505c19eb3d9bb009f38c
Jtiser <julien.tiser@outlook.com>
2018-01-14 16:01:04 +0100

    Adding more Test

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -24,4 +24,9 @@ describe('<NavigationItems />', () => {
         wrapper.setProps({ isAuthenticated: true });
         expect(wrapper.find(NavigationItem)).toHaveLength(3);
     });
+
+    it('should render one <NavigationItem /> element with Logout label if authenticated', () => {
+        wrapper.setProps({ isAuthenticated: true });
+        expect(wrapper.contains(<NavigationItem link='/logout'>Logout</NavigationItem>)).toEqual(true);
+    });
 });
